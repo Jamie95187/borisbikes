45b40dbec38a296f87f74bb4cf4e9d48a4fb4945
Jamie95187 <jamiewong63@googlemail.com>
2019-12-24 20:23:41 +0000

    refactor to use private methods to keep SRP

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -8,12 +8,25 @@ class DockingStation
   end
 
   def release_bike
-    fail "No bikes available" unless !@bikes.empty?
+    fail "No bikes available" if empty?
+
     @bikes.pop
   end
 
   def dock_bike(bike)
-    fail "Already at max capacity" unless @bikes.size < 20
+    fail "Already at max capacity" if full?
+    
     @bikes << bike
   end
+
+  private
+
+  def full?
+    @bikes.count == 20
+  end
+
+  def empty?
+    @bikes.count.zero?
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -19,14 +19,14 @@ describe DockingStation do
     end
 
     it 'should raise an error if there are no bikes available to release' do
-      expect{ subject.release_bike }.to raise_error("No bikes available")
+      expect { subject.release_bike }.to raise_error("No bikes available")
     end
 
   end
 
   describe '#dock_bike' do
 
-    it{is_expected.to respond_to(:dock_bike).with(1).argument}
+    it { is_expected.to respond_to(:dock_bike).with(1).argument }
 
     it 'should add a bike to the bikes' do
       bike = Bike.new
@@ -35,8 +35,8 @@ describe DockingStation do
     end
 
     it 'should raise error if the station is already at max capacity' do
-      20.times{ subject.dock_bike Bike.new }
-      expect{ subject.dock_bike Bike.new }.to raise_error "Already at max capacity"
+      20.times { subject.dock_bike Bike.new }
+      expect { subject.dock_bike Bike.new }.to raise_error "Already at max capacity"
     end
 
   end
